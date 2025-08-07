261b40f5366fe1342c392c19c2f4820e66961641
GAlexCarrion <alexcrap95@gmail.com>
2025-08-07 07:59:46 -0500

    Actualizacion

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { CarruselComponent } from '../../components/carrusel/carrusel'; 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CarruselComponent], 
+  imports: [CommonModule, CarruselComponent], 
   templateUrl: './home.html',
-  styleUrls: ['./home.css']
+  styleUrls: ['./home.css'] 
 })
 export class HomeComponent {
-  welcomeMessage: string = 'Explora nuestra vasta colección de joyas del rock.';
+  welcomeMessage: string = 'Donde cada surco cuenta una historia y cada nota resuena con el alma del rock. Sumérgete en la leyenda.';
 }
